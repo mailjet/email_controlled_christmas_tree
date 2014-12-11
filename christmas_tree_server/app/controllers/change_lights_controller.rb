31cbd017133f71664989e64135521c3308a30b6d
Tyler Nappy <tylernappy@gmail.com>
2014-12-11 15:20:53 -0500

    updated output on logs when email is sentin

--- christmas_tree_server/app/controllers/change_lights_controller.rb
+++ christmas_tree_server/app/controllers/change_lights_controller.rb
@@ -1,31 +1,35 @@
 class ChangeLightsController < ApplicationController
-   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
-   def email_received
-     words = params["Text-part"].split(" ")
-     lights = words[(words.index("lights")+1)].downcase unless words.index("lights").nil?
-     icon = words[(words.index("icon")+1)].downcase unless words.index("icon").nil?
-     background = words[(words.index("background")+1)].downcase unless words.index("background").nil?
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+  def email_received
+    words = params["Text-part"].split(" ")
+    lights = words[(words.index("lights")+1)].downcase unless words.index("lights").nil?
+    icon = words[(words.index("icon")+1)].downcase unless words.index("icon").nil?
+    background = words[(words.index("background")+1)].downcase unless words.index("background").nil?
 
-     #POST request for LED strip
-     unless lights.nil?
-        HTTParty.post("https://api.spark.io/v1/devices/#{ENV['coreID']}/led_strip", body: {access_token: ENV['accessToken'], lights: lights})
-      end
+    puts "Email came from: #{params["From"]}"
+    puts "Text part: #{params["Text-part"]}"
+    puts "----------------------------------------------------"
 
-      #POST request for LED background color
-      unless background.nil?
-        # icon = format_icon(params["Text-part"])
-        HTTParty.post("https://api.spark.io/v1/devices/#{ENV['coreID']}/background", body: {access_token: ENV['accessToken'], background: background})
-      end
+    #POST request for LED strip
+    unless lights.nil?
+      HTTParty.post("https://api.spark.io/v1/devices/#{ENV['coreID']}/led_strip", body: {access_token: ENV['accessToken'], lights: lights})
+    end
 
-      #POST request for LED dot matrix
-      unless icon.nil?
-        # icon = format_icon(params["Text-part"])
-        HTTParty.post("https://api.spark.io/v1/devices/#{ENV['coreID']}/dot_matrix", body: {access_token: ENV['accessToken'], icon: icon})
-      end
-   end
+    #POST request for LED background color
+    unless background.nil?
+      # icon = format_icon(params["Text-part"])
+      HTTParty.post("https://api.spark.io/v1/devices/#{ENV['coreID']}/background", body: {access_token: ENV['accessToken'], background: background})
+    end
 
-   private
-   def format_icon(text)
+    #POST request for LED dot matrix
+    unless icon.nil?
+      # icon = format_icon(params["Text-part"])
+      HTTParty.post("https://api.spark.io/v1/devices/#{ENV['coreID']}/dot_matrix", body: {access_token: ENV['accessToken'], icon: icon})
+    end
+  end
 
-   end
+  private
+  def format_icon(text)
+
+  end
 end
