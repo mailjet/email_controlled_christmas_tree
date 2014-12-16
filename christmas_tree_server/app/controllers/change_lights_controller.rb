fb722ff9347df7c92210800e4a3f94afe2776b70
Tyler Nappy <tylernappy@gmail.com>
2014-12-16 10:44:28 -0500

    updated to accomdate uppercase commands

--- christmas_tree_server/app/controllers/change_lights_controller.rb
+++ christmas_tree_server/app/controllers/change_lights_controller.rb
@@ -1,7 +1,7 @@
 class ChangeLightsController < ApplicationController
   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
   def email_received
-    words = params["Text-part"].split(" ")
+    words = params["Text-part"].split(" ").map{|word| word.downcase}
     lights = words[(words.index("lights")+1)].downcase unless words.index("lights").nil?
     icon = words[(words.index("icon")+1)].downcase unless words.index("icon").nil?
     background = words[(words.index("background")+1)].downcase unless words.index("background").nil?
@@ -21,11 +21,15 @@ class ChangeLightsController < ApplicationController
       HTTParty.post("https://api.spark.io/v1/devices/#{ENV['coreID']}/background", body: {access_token: ENV['accessToken'], background: background})
     end
 
-    #POST request for LED dot matrix
+    #POST request for LED dot matrix icon
     unless icon.nil?
       # icon = format_icon(params["Text-part"])
       HTTParty.post("https://api.spark.io/v1/devices/#{ENV['coreID']}/dot_matrix", body: {access_token: ENV['accessToken'], icon: icon})
     end
+  end
+
+  def index
+
   end
 
   private
